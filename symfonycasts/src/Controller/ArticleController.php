31836b8df06f1e1da7cec386fd232112ab14d41f
gasiorradek@gmail.com <gasiorradek@gmail.com>
2019-08-14 15:26:27 +0200

    twig added

--- symfonycasts/src/Controller/ArticleController.php
+++ symfonycasts/src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,7 +23,9 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf('Future page to show: %s', $slug));
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+        ]);
     }
 
 }
